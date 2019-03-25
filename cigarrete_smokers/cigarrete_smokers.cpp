431bd184e02018e31ed4485b005fcb7da7261413
Nikunj Gupta <nk.nikunjgupta@gmail.com>
2019-03-25 20:37:16 +0530

    Completes cigarette smokers implementation
    Signed-off-by: Nikunj Gupta <nk.nikunjgupta@gmail.com>

--- cigarrete_smokers/cigarrete_smokers.cpp
+++ cigarrete_smokers/cigarrete_smokers.cpp
@@ -12,12 +12,16 @@
 #include <utility>
 #include <ctime>
 #include <string>
+#include <memory>
 
+///////////////////////////////////////////////////////////////////////////////
 #define TOBACO 1
 #define PAPER 2
 #define MATCH 3
 
-hpx::lcos::local::mutex smoke, tobaco, paper, match, check;
+hpx::lcos::local::mutex smoke, check;
+
+bool should_stop = false;
 
 struct smoker
 {
@@ -31,19 +35,7 @@ struct smoker
         has_match(m) {}
 };
 
-// smoker1 has infinite tobaco
-smoker smoker1 {1, 0, 0};
-
-// smoker2 has infinite papers
-smoker smoker2 {0, 1, 0};
-
-// smoker3 has infinite matches
-smoker smoker3 {0, 0, 1};
-
-int prod1;
-int prod2;
-bool change = false;
-
+///////////////////////////////////////////////////////////////////////////////
 std::pair<int, int> generate_two_products()
 {
     std::srand(std::time(0));
@@ -63,53 +55,65 @@ std::pair<int, int> generate_two_products()
     }
 }
 
-void smoke_cigarrete(smoker s)
+///////////////////////////////////////////////////////////////////////////////
+void smoke_cigarrete(std::shared_ptr<smoker> s)
 {
     std::string str = "";
-    if(s.has_tobaco)    str += "smoker with tobaco";
-    if(s.has_paper)    str += "smoker with paper";
-    if(s.has_match)    str += "smoker with match";
+    if(s -> has_tobaco)    str += "smoker with tobaco";
+    if(s -> has_paper)    str += "smoker with paper";
+    if(s -> has_match)    str += "smoker with match";
     std::cout << str << " is now smoking.." << std::endl;
     hpx::this_thread::sleep_for(std::chrono::milliseconds(100));
 }
 
-void create_cigarrete_tobacco()
+///////////////////////////////////////////////////////////////////////////////
+void create_cigarrete_tobacco(
+    std::shared_ptr<smoker> s,
+    std::shared_ptr<int> prod1,
+    std::shared_ptr<int> prod2,
+    std::shared_ptr<bool> change
+)
 {
-    while(1)
+    while(!should_stop)
     {
         {
             {
                 std::lock_guard<hpx::lcos::local::mutex> lock(check);
-                if((prod1 == MATCH || prod1 == PAPER) 
+                if((*prod1 == MATCH || *prod1 == PAPER) 
                     && 
-                    ((prod2 == MATCH || prod2 == PAPER)))
+                    ((*prod2 == MATCH || *prod2 == PAPER)))
                 {
                     std::lock_guard<hpx::lcos::local::mutex> lock(smoke);
-                    smoke_cigarrete(smoker1);
+                    smoke_cigarrete(s);
                 }
             }
-            change = true;
+            *change = true;
             hpx::this_thread::sleep_for(std::chrono::milliseconds(50));
         }
     }
 }
 
-void create_cigarrete_paper()
+void create_cigarrete_paper(
+    std::shared_ptr<smoker> s,
+    std::shared_ptr<int> prod1,
+    std::shared_ptr<int> prod2,
+    std::shared_ptr<bool> change
+)
 {
-    while(1)
+    while(!should_stop)
     {
         {
             {
                 std::lock_guard<hpx::lcos::local::mutex> lock(check);
-                if((prod1 == MATCH || prod1 == TOBACO) 
+                if((*prod1 == MATCH || *prod1 == TOBACO) 
                     && 
-                    ((prod2 == MATCH || prod2 == TOBACO)))
+                    ((*prod2 == MATCH || *prod2 == TOBACO)))
                 {
                     std::lock_guard<hpx::lcos::local::mutex> lock(smoke);
-                    smoke_cigarrete(smoker2);
+                    smoke_cigarrete(s);
                 }
             }    
-            change = true;
+            *change = true;
             hpx::this_thread::sleep_for(std::chrono::milliseconds(50));
         }
         // change = true;
@@ -117,22 +121,27 @@ void create_cigarrete_paper()
     }
 }
 
-void create_cigarrete_match()
+void create_cigarrete_match(
+    std::shared_ptr<smoker> s,
+    std::shared_ptr<int> prod1,
+    std::shared_ptr<int> prod2,
+    std::shared_ptr<bool> change
+)
 {
-    while(1)
+    while(!should_stop)
     {
         {
             {
                 std::lock_guard<hpx::lcos::local::mutex> lock(check);
-                if((prod1 == PAPER || prod1 == TOBACO) 
+                if((*prod1 == PAPER || *prod1 == TOBACO) 
                     && 
-                    ((prod2 == PAPER || prod2 == TOBACO)))
+                    ((*prod2 == PAPER || *prod2 == TOBACO)))
                 {
                     std::lock_guard<hpx::lcos::local::mutex> lock(smoke);
-                    smoke_cigarrete(smoker3);
+                    smoke_cigarrete(s);
                 }
             }
-            change = true;
+            *change = true;
             hpx::this_thread::sleep_for(std::chrono::milliseconds(50));
         }
         // change = true;
@@ -140,26 +149,42 @@ void create_cigarrete_match()
     }
 }
 
-void simulate()
+///////////////////////////////////////////////////////////////////////////////
+void stop_threads()
+{
+    std::cin.get();
+    should_stop = true; 
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void simulate(std::shared_ptr<smoker> s1,
+              std::shared_ptr<smoker> s2,
+              std::shared_ptr<smoker> s3)
 {
     std::pair<int, int> p = generate_two_products();
 
-    prod1 = std::get<0>(p);
-    prod2 = std::get<1>(p);
+    std::shared_ptr<int> prod1 = std::make_shared<int>();
+    std::shared_ptr<int> prod2 = std::make_shared<int>();
+    std::shared_ptr<bool> change = std::make_shared<bool>();
 
-    hpx::thread t1(&create_cigarrete_tobacco);
-    hpx::thread t2(&create_cigarrete_paper);
-    hpx::thread t3(&create_cigarrete_match);
+    *prod1 = std::get<0>(p);
+    *prod2 = std::get<1>(p);
 
-    while(1)
+    hpx::thread t1(&create_cigarrete_tobacco, s1, prod1, prod2, change);
+    hpx::thread t2(&create_cigarrete_paper, s2, prod1, prod2, change);
+    hpx::thread t3(&create_cigarrete_match, s3, prod1, prod2, change);
+
+    hpx::thread stop(&stop_threads);
+
+    while(!should_stop)
     {
-        if(change)
+        if(*change)
         {
             std::lock_guard<hpx::lcos::local::mutex> lock(check);
             p = generate_two_products();
 
-            prod1 = std::get<0>(p);
-            prod2 = std::get<1>(p);
+            *prod1 = std::get<0>(p);
+            *prod2 = std::get<1>(p);
             // hpx::this_thread::sleep_for(std::chrono::milliseconds(30));
         }
     }
@@ -167,11 +192,16 @@ void simulate()
     t1.join();
     t2.join();
     t3.join();
+    stop.join();
 }
 
 int main(int argc, char* argv[])
 {
-    simulate();
+    std::shared_ptr<smoker> smoker1 = std::make_shared<smoker>(1, 0, 0);
+    std::shared_ptr<smoker> smoker2 = std::make_shared<smoker>(0, 1, 0);
+    std::shared_ptr<smoker> smoker3 = std::make_shared<smoker>(0, 0, 1);
+
+    simulate(smoker1, smoker2, smoker3);
 
     return 0;
 }
